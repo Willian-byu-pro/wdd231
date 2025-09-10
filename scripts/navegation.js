f35c1eb30a6a4ad140708ea34e0f73210e7d7e6d
Willian-byu-pro <willianramosdida@gmail.com>
2025-09-09 21:34:56 -0300

    Update

--- scripts/navegation.js
+++ scripts/navegation.js
@@ -28,6 +28,22 @@ navbutton.addEventListener ('click', () => {
 });
 
 
+// ----------------------------- laste Modificação ------------------------------------------------------------------------
+
+
+//Aqui ele está pegando o elemento da página HTML com o id especificado.//
+const LastModified = document.querySelector("#LastModified");
+
+//Isso cria um objeto de data atual, ou seja, o dia e a hora exatos de agora.//
+const today = new Date();
+
+
+LastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.DateTimeFormat("en-US",
+{
+    dateStyle:"full"
+
+}
+).format(today)}</span>`;
 
 
 
